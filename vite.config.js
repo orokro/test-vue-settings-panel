b2faf55ff76778d6ebee444807e73f9c4409ccb5
Greg Miller <hypurban86@gmail.com>
2026-03-28 21:46:11 -0400

    Changes build settings for github.io

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Base path for hosting on GitHub Pages (makes paths relative)
+  base: './',
   plugins: [
     vue(),
     vueDevTools(),
